df00270d6d02d37508254703d7feca4b42246808
jtraverso <jtraverso@users.noreply.github.com>
2022-02-04 09:54:02 -0300

    Update AboutModal.tsx

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -20,7 +20,7 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
       <p className="text-sm text-gray-500 dark:text-gray-300">
        Basdo en -{' '}
         <a
-          href="https://github.com/hannahcode/GAME"
+          "href="https://github.com/hannahcode/GAME"
           className="underline font-bold"
         >
           revisa el código aquí
