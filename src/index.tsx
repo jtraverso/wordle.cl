878ee72daa3a1c19e9904094b20b8365f441de47
jtraverso <jtraverso@users.noreply.github.com>
2022-02-04 13:09:06 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
