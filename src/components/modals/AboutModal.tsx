5a6f7e7eed1e4b5be618803755850bf0e4975452
jtraverso <jtraverso@users.noreply.github.com>
2022-02-04 09:24:46 -0300

    Update AboutModal.tsx

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -9,12 +9,21 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        This is an open source word guessing game -{' '}
+        Este juego es Open Source -{' '}
+        <a
+          "https://github.com/jtraverso/wordle.cl"
+          className="underline font-bold"
+        >
+          revisa el código aquí
+        </a>{' '}
+      </p>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+       Basdo en -{' '}
         <a
           href="https://github.com/hannahcode/GAME"
           className="underline font-bold"
         >
-          check out the code here
+          revisa el código aquí
         </a>{' '}
       </p>
     </BaseModal>
