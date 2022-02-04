d8a66f58de0c72ac8d3904dece34d9a753c6e235
jtraverso <jtraverso@users.noreply.github.com>
2022-02-04 09:31:40 -0300

    Update InfoModal.tsx

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,41 +10,41 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the word in 6 tries. After each guess, the color of the tiles will
-        change to show how close your guess was to the word.
+        Tienes 6 intentos para adivinar la palabra (incluye ciudades, comunas, modismos y otras expresiones típicas chilenas.
+        Despuess de cada intento cada celda cambiará según que tan cerca estuviste.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="W" status="correct" />
+        <Cell value="C" status="correct" />
+        <Cell value="H" />
+        <Cell value="I" />
+        <Cell value="L" />
         <Cell value="E" />
-        <Cell value="A" />
-        <Cell value="R" />
-        <Cell value="Y" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
+        The letter C is está en la palabra y en el lugar correcto.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
         <Cell value="P" />
-        <Cell value="I" />
-        <Cell value="L" status="present" />
-        <Cell value="O" />
-        <Cell value="T" />
+        <Cell value="L" />
+        <Cell value="U" status="present" />
+        <Cell value="G" />
+        <Cell value="A" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
+        The letra U está en la palabra, pero en el lugar incorrecto.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
         <Cell value="A" />
-        <Cell value="G" />
-        <Cell value="U" status="absent" />
-        <Cell value="E" />
+        <Cell value="B" />
+        <Cell value="A" />
+        <Cell value="C" status="absent" />
+        <Cell value="O" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
+        The letra C no está en la palabra.
       </p>
     </BaseModal>
   )
