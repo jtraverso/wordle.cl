aad7154e71b79b557a121b7484d3546e8b8e5a09
jtraverso <jtraverso@users.noreply.github.com>
2022-02-05 11:04:51 -0300

    Update usePageTracking.ts

--- src/lib/usePageTracking.ts
+++ src/lib/usePageTracking.ts
@@ -4,9 +4,6 @@ import ReactGA from "react-ga"
 
 export const usePageTracking = () => {
   const location = useLocation()
-
-  useEffect(() => {
-    ReactGA.initialize("G-GR99QS3PLS")
-    ReactGA.pageview(location.pathname + location.search)
-  }, [location])
+  ReactGA.initialize("G-GR99QS3PLS")
+  ReactGA.pageview(location.pathname + location.search)
 }
