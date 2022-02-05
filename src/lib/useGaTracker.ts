a2d260544924a0cd6071ee1ff08ab9b206b1c1c9
jtraverso <jtraverso@users.noreply.github.com>
2022-02-05 15:32:28 -0300

    Update and rename usePageTracking.ts to useGaTracker.ts

---
+++ src/lib/useGaTracker.ts
@@ -0,0 +1,21 @@
+import { useEffect} from "react"
+import { useLocation } from "react-router-dom"
+import ReactGA from "react-ga"
+
+const useGaTracker = () => {
+  const location = useLocation()
+  const [initialized, setInitialized] = useState(false)
+
+  useEffect(() => {
+    ReactGA.initialize("G-GR99QS3PLS")
+    setInitialized(true)
+    }, [setInitialized])
+ 
+  useEffect(() => {
+    if (initialized) {
+      ReactGA.pageview(location.pathname + location.search)
+    }
+  },[initialized, location])
+}
+
+export default useGaTracker
--- src/lib/usePageTracking.ts
+++
@@ -1,9 +0,0 @@
-import { useEffect} from "react"
-import { useLocation } from "react-router-dom"
-import ReactGA from "react-ga"
-
-export const usePageTracking = () => {
-  const location = useLocation()
-  ReactGA.initialize("G-GR99QS3PLS")
-  ReactGA.pageview(location.pathname + location.search)
-}
