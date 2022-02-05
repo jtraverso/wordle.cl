fa0a8e53f5cea71e576925bb6520740d7d14b110
jtraverso <jtraverso@users.noreply.github.com>
2022-02-05 10:04:34 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import { Keyboard } from './components/keyboard/Keyboard'
 import { AboutModal } from './components/modals/AboutModal'
 import { InfoModal } from './components/modals/InfoModal'
 import { StatsModal } from './components/modals/StatsModal'
+import { usePageTracking } from from './lib/usePageTracking'
 import {
   GAME_TITLE,
   WIN_MESSAGES,
