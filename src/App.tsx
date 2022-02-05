a413ff78c75e3198fc21ac097c26b4208750f2e2
jtraverso <jtraverso@users.noreply.github.com>
2022-02-05 16:38:51 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -151,7 +151,7 @@ function App() {
   }
 
   return (
-    <div><useGaTracker /></div>
+    <useGaTracker />
     <div className="py-8 max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div className="flex w-80 mx-auto items-center mb-8 mt-12">
         <h1 className="text-xl grow font-bold dark:text-white">{GAME_TITLE}</h1>
