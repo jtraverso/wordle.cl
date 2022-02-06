c3357f62aca4484a8925eae26e133bc420671026
jtraverso <jtraverso@users.noreply.github.com>
2022-02-06 01:58:02 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -26,7 +26,7 @@ ReactGA.pageview(window.location.pathname) // track page view
 
 const script = document.createElement("script")
 script.async = true
-script.text = ´<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2955738892636836" crossorigin="anonymous"></script>´
+script.text = '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2955738892636836" crossorigin="anonymous"></script>'
 document.head.appendChild(script)
 
 ReactDOM.render(
