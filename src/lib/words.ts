675656719c23dce672ac209c0eb2257b3aecb264
jtraverso <jtraverso@users.noreply.github.com>
2022-02-07 09:54:55 -0300

    Update words.ts

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -16,9 +16,9 @@ export const getWordOfDay = () => {
   // January 1, 2022 Game Epoch
   const epochMs = new Date('January 1, 2022 00:00:00').valueOf()
   const now = Date.now()
-  const msInDay = 28800000
+  const msInDay = 28800000 //modified to play every 8 hours (8*60*60*1000)
   const index = Math.floor((now - epochMs) / msInDay)
-  const nextday = (index + 1) * msInDay + epochMs
+  const nextday = (index + 1) * msInDay + epochMs - 10.800 //10.800 (3*60*60) is to rest 3 hours to the next play, so it suits better the chilean time
 
   return {
     solution: WORDS[index % WORDS.length].toUpperCase(),
