deb0c74753e2a9509a7764ff4d8ff92dcf6396f6
jtraverso <jtraverso@users.noreply.github.com>
2022-02-08 16:35:39 -0300

    Update share.ts

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -1,11 +1,14 @@
 import { getGuessStatuses } from './statuses'
 import { solutionIndex } from './words'
 import { GAME_TITLE, GAME_URL } from '../constants/strings'
+import { shareButtons } from './shareButtons'
+
+const sb = () => <shareButtons />
 
 export const shareStatus = (guesses: string[], lost: boolean) => {
   navigator.clipboard.writeText(
     `${GAME_TITLE} ${solutionIndex} ${lost ? 'X' : guesses.length}/6\n${GAME_URL}\n\n` +
-      generateEmojiGrid(guesses)
+      generateEmojiGrid(guesses) + `\n\n${sb}`
   )
 }
 
