5794e137f1b1464634c7316841eb2257f7f224da
jtraverso <jtraverso@users.noreply.github.com>
2022-02-08 15:11:33 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -151,8 +151,8 @@ function App() {
   }
 
   return (
-    <div className="py-8 max-w-7xl mx-auto sm:px-6 lg:px-8">
-      <div className="flex w-80 mx-auto items-center mb-8 mt-12">
+    <div className="py-8 max-w-7xl mx-auto sm:px-6 lg:px-8 wordle-padding">
+      <div className="flex w-80 mx-auto items-center mb-8 mt-12 wordle-margin">
         <h1 className="text-xl grow font-bold dark:text-white">{GAME_TITLE}</h1>
         {isDarkMode ? (
           <SunIcon
