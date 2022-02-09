9e8329c2be34e656941eac512f2a4c8ff95411a8
jtraverso <jtraverso@gmail.com>
2022-02-09 17:56:58 -0300

    ok

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -9,10 +9,6 @@ export const shareStatus = (guesses: string[], lost: boolean) => {
   )
 }
 
-export const shareText = (guesses: string[], lost: boolean, game_title: string, game_url: string) => {
-    return `${game_title} ${solutionIndex} ${lost ? 'X' : guesses.length}/6\n${game_url}\n` + generateEmojiGrid(guesses)
-}
-
 export const generateEmojiGrid = (guesses: string[]) => {
 return guesses
     .map((guess) => {
