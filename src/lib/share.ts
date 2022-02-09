ac2c0051f864cd1215d83b4ec112250d9e0b166c
jtraverso <jtraverso@gmail.com>
2022-02-09 16:15:27 -0300

    develop

---
+++ src/lib/share.ts
@@ -0,0 +1,36 @@
+import { getGuessStatuses } from './statuses'
+import { solutionIndex } from './words'
+import { GAME_TITLE, GAME_URL } from '../constants/strings'
+
+export const shareStatus = (guesses: string[], lost: boolean) => {
+  navigator.clipboard.writeText(
+    `${GAME_TITLE} ${solutionIndex} ${lost ? 'X' : guesses.length}/6\n${GAME_URL}\n\n` +
+      generateEmojiGrid(guesses)
+  )
+}
+/*
+export const shareText = (guesses: string[], lost: boolean, game_title: string, game_url: string) => {
+    return `${game_title} ${solutionIndex} ${lost ? 'X' : guesses.length}/6\n${game_url}\n` + generateEmojiGrid(guesses)
+}
+*/
+
+export const generateEmojiGrid = (guesses: string[]) => {
+return guesses
+    .map((guess) => {
+      const status = getGuessStatuses(guess)
+      return guess
+        .split('')
+        .map((_, i) => {
+          switch (status[i]) {
+            case 'correct':
+              return '🟩'
+            case 'present':
+              return '🟦'
+            default:
+              return '🟥'
+          }
+        })
+        .join('')
+    })
+    .join('\n')
+}
