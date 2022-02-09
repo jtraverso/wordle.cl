bffe5235f94154bbff8b7b8cc6e112134e8811c6
jtraverso <jtraverso@users.noreply.github.com>
2022-02-09 10:58:31 -0300

    Update shareButtons.tsx

--- src/components/shareButtons/shareButtons.tsx
+++ src/components/shareButtons/shareButtons.tsx
@@ -1,4 +1,4 @@
-import { GAME_TITLE, GAME_URL } from '../constants/strings'
+import React from 'react'
 import {
   WhatsappShareButton,
   FacebookShareButton,
@@ -14,48 +14,11 @@ import {
 } from 'react-share'
 import './shareButtons.css'
 
-
-navigator.clipboard.writeText(  WhatsappShareButton + FacebookShareButton + FacebookMessengerShareButton +TwitterShareButton + TelegramShareButton + 
-                              FacebookShareCount + WhatsappIcon + FacebookIcon + FacebookMessengerIcon + TwitterIcon+  TelegramIcon)
-navigator.clipboard.writeText("")
-
-
-import { getGuessStatuses } from '../../lib/statuses'
-import { Cell } from './Cell'
-
-type Props = {
-  guess: string
-}
-
-export const CompletedRow1 = ({ guess }: Props) => {
-  const statuses = getGuessStatuses(guess)
-
-  return (
-    <div className="flex justify-center mb-1">
-      {guess.split('').map((letter, i) => (
-        <Cell key={i} value={letter} status={statuses[i]} />
-      ))}
-    </div>
-  )
-}
-
-/*
-
 type Props = {
   title: string
   shareUrl: string
 }
 
-
-
-export const ShareButtons = ({ title, shareUrl }: Props) => {
-  return (
-    <div className="flex justify-center mb-1">
-      { title}
-      { title}
-    </div>
-  )
-}
 export const ShareButtons = ({ title, shareUrl }: Props) => {
   return (
     <div className="Demo__container">
