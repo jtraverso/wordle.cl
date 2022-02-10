e4db5aa7372ec7fa32cf04deaebf69529f8dd44b
jtraverso <jtraverso@gmail.com>
2022-02-10 12:12:23 -0300

    ok

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,19 +1,19 @@
-export const GAME_TITLE = 'WORDLE.CL'
-export const GAME_URL = 'https://www.wordle.cl'
-export const WIN_MESSAGES = ['¡Excelente!', '¡Increible!', '¡Bien Hecho!', '¡Eres Formidable!', '¡Sec@!','¡Bakan!','¡La Raja!','¡Impresionante','¡Te Pasaaaaaaste!' ]
-export const GAME_COPIED_MESSAGE = 'Copiado al portapeles'
-export const ABOUT_GAME_MESSAGE = 'Acerca de este juego'
-export const NOT_ENOUGH_LETTERS_MESSAGE = 'No hay suficientes letras'
-export const WORD_NOT_FOUND_MESSAGE = 'Palabra no encontrada'
+export const GAME_TITLE = 'Reactle'
+
+export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
+export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
+export const ABOUT_GAME_MESSAGE = 'About this game'
+export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
+export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
-  `La palabra era ${solution}`
+  `The word was ${solution}`
 export const ENTER_TEXT = 'Enter'
-export const DELETE_TEXT = 'Borrar'
-export const STATISTICS_TITLE = 'Estadísticas'
-export const GUESS_DISTRIBUTION_TEXT = 'Tu distribución'
-export const NEW_WORD_TEXT = 'Nueva palabra en'
-export const SHARE_TEXT = 'COPIAR RESULTADO'
-export const TOTAL_TRIES_TEXT = 'Intentos totales'
-export const SUCCESS_RATE_TEXT = 'Tasa de éxito'
-export const CURRENT_STREAK_TEXT = 'Racha actual'
-export const BEST_STREAK_TEXT = 'Mejor racha'
+export const DELETE_TEXT = 'Delete'
+export const STATISTICS_TITLE = 'Statistics'
+export const GUESS_DISTRIBUTION_TEXT = 'Guess Distribution'
+export const NEW_WORD_TEXT = 'New word in'
+export const SHARE_TEXT = 'Share'
+export const TOTAL_TRIES_TEXT = 'Total tries'
+export const SUCCESS_RATE_TEXT = 'Success rate'
+export const CURRENT_STREAK_TEXT = 'Current streak'
+export const BEST_STREAK_TEXT = 'Best streak'
