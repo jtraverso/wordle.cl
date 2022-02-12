15a70532dcb59de3e93dccdcafac3c2fa2510b46
jtraverso <jtraverso@gmail.com>
2022-02-12 13:06:45 -0300

    ok

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -5,6 +5,7 @@ import { GameStats } from '../../lib/localStorage'
 import { shareStatus } from '../../lib/share'
 import { tomorrow } from '../../lib/words'
 import { BaseModal } from './BaseModal'
+import {InlineShareButtons} from 'sharethis-reactjs'
 import {
   STATISTICS_TITLE,
   GUESS_DISTRIBUTION_TEXT,
@@ -75,6 +76,34 @@ export const StatsModal = ({
           >
             {SHARE_TEXT}
           </button>
+		  <InlineShareButtons
+          config={{
+            alignment: 'center',  // alignment of buttons (left, center, right)
+            color: 'social',      // set the color of buttons (social, white)
+            enabled: true,        // show/hide buttons (true, false)
+            font_size: 16,        // font size for the buttons
+            labels: 'cta',        // button labels (cta, counts, null)
+            language: 'es',       // which language to use (see LANGUAGES)
+            networks: [           // which networks to include (see SHARING NETWORKS)
+              'whatsapp',
+			  'facebook',
+              'messenger',
+              'twitter',
+			  'telegram',
+			  'reddit',
+            ],
+            padding: 12,          // padding within buttons (INTEGER)
+            radius: 4,            // the corner radius on each button (INTEGER)
+            show_total: true,
+            size: 32,             // the size of each button (INTEGER)
+
+            // OPTIONAL PARAMETERS
+            //url: '', // (defaults to current url)
+            //image: 'https://bit.ly/2CMhCMC',  // (defaults to og:image or twitter:image)
+            description: 'Wordle a la chilena',       // (defaults to og:description or twitter:description)
+            title: 'Wordle.cl - Adivina la palabra en 6 intentos',            // (defaults to og:title or twitter:title)
+          }}
+        />
         </div>
       )}
     </BaseModal>
