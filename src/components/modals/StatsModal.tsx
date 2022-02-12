2ecbb24f50ea80864dfe0c0888ebd10e58acbb7c
jtraverso <jtraverso@users.noreply.github.com>
2022-02-12 14:35:57 -0300

    Add files via upload

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -2,9 +2,10 @@ import Countdown from 'react-countdown'
 import { StatBar } from '../stats/StatBar'
 import { Histogram } from '../stats/Histogram'
 import { GameStats } from '../../lib/localStorage'
-import { shareStatus } from '../../lib/share'
+import { shareStatus, shareText } from '../../lib/share'
 import { tomorrow } from '../../lib/words'
 import { BaseModal } from './BaseModal'
+import {InlineShareButtons} from 'sharethis-reactjs'
 import {
   STATISTICS_TITLE,
   GUESS_DISTRIBUTION_TEXT,
@@ -49,13 +50,14 @@ export const StatsModal = ({
       title={STATISTICS_TITLE}
       isOpen={isOpen}
       handleClose={handleClose}
-    >
+	>
       <StatBar gameStats={gameStats} />
       <h4 className="text-lg leading-6 font-medium text-gray-900 dark:text-gray-100">
         {GUESS_DISTRIBUTION_TEXT}
       </h4>
       <Histogram gameStats={gameStats} />
       {(isGameLost || isGameWon) && (
+	  <div>
         <div className="mt-5 sm:mt-6 columns-2 dark:text-white">
           <div>
             <h5>{NEW_WORD_TEXT}</h5>
@@ -75,7 +77,35 @@ export const StatsModal = ({
           >
             {SHARE_TEXT}
           </button>
+		</div>
+		<div className="mt-5 sm:mt-6 columns-1 dark:text-white">
+		  <InlineShareButtons
+          config={{
+            alignment: 'center',  // alignment of buttons (left, center, right)
+            color: 'social',      // set the color of buttons (social, white)
+            enabled: true,        // show/hide buttons (true, false)
+            font_size: 16,        // font size for the buttons
+            labels: 'cta',        // button labels (cta, counts, null)
+            language: 'es',       // which language to use (see LANGUAGES)
+            networks: [           // which networks to include (see SHARING NETWORKS)
+              'whatsapp',
+              'twitter',
+			  'reddit',
+            ],
+            padding: 12,          // padding within buttons (INTEGER)
+            radius: 4,            // the corner radius on each button (INTEGER)
+            show_total: true,
+            size: 32,             // the size of each button (INTEGER)
+
+            // OPTIONAL PARAMETERS
+            url: shareText(guesses, isGameLost, isHardMode), // (defaults to current url)
+            //image: 'https://bit.ly/2CMhCMC',  // (defaults to og:image or twitter:image)
+			//description: shareText(guesses, isGameLost, isHardMode),       // (defaults to og:description or twitter:description)
+            title: 'Wordle.cl - Adivina la palabra en 6 intentos',            // (defaults to og:title or twitter:title)
+          }}
+        />
         </div>
+	    </div>
       )}
     </BaseModal>
   )
