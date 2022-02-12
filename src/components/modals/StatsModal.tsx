c91918c194fe1eb625f962865fe3b98301ae1d83
jtraverso <jtraverso@gmail.com>
2022-02-12 14:05:14 -0300

    ok

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -106,6 +106,31 @@ export const StatsModal = ({
             title: 'Wordle.cl - Adivina la palabra en 6 intentos',            // (defaults to og:title or twitter:title)
           }}
         />
+	    <InlineShareButtons
+          config={{
+            alignment: 'center',  // alignment of buttons (left, center, right)
+            color: 'social',      // set the color of buttons (social, white)
+            enabled: true,        // show/hide buttons (true, false)
+            font_size: 16,        // font size for the buttons
+            labels: 'cta',        // button labels (cta, counts, null)
+            language: 'es',       // which language to use (see LANGUAGES)
+            networks: [           // which networks to include (see SHARING NETWORKS)
+			  'facebook',
+              'messenger',
+            ],
+            padding: 12,          // padding within buttons (INTEGER)
+            radius: 4,            // the corner radius on each button (INTEGER)
+            show_total: true,
+            size: 32,             // the size of each button (INTEGER)
+
+            // OPTIONAL PARAMETERS
+            //url: '', // (defaults to current url)
+            //image: '',  // (defaults to og:image or twitter:image)
+            description: shareText(guesses, isGameLost, isHardMode),       // (defaults to og:description or twitter:description)
+            title: 'Wordle.cl - Adivina la palabra en 6 intentos',            // (defaults to og:title or twitter:title)
+          }}
+        />
+        </div>
         </div>
 	    </div>
       )}
