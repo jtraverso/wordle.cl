7a5060e700bb21c2676c9be4505d174e1923768d
jtraverso <jtraverso@users.noreply.github.com>
2022-02-12 12:42:42 -0300

    Add files via upload

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -2,7 +2,7 @@ export const GAME_TITLE = 'WORDLE.CL'
 export const GAME_URL = 'https://www.wordle.cl'
 export const WIN_MESSAGES = ['¡Excelente!', '¡Increible!', '¡Bien Hecho!', '¡Eres Formidable!', '¡Sec@!','¡Bakan!','¡La Raja!','¡Impresionante','¡Te Pasaaaaaaste!' ]
 export const GAME_COPIED_MESSAGE = 'Copiado al portapeles'
-export const ABOUT_GAME_MESSAGE = 'Acerca de este juego'
+export const ABOUT_GAME_MESSAGE = 'Sobre wordle.cl'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'No hay suficientes letras'
 export const WORD_NOT_FOUND_MESSAGE = 'Palabra no encontrada'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
@@ -18,3 +18,4 @@ export const SUCCESS_RATE_TEXT = 'Tasa de éxito'
 export const CURRENT_STREAK_TEXT = 'Racha actual'
 export const BEST_STREAK_TEXT = 'Mejor racha'
 export const HOW_TO_PLAY_TEXT = 'Como jugar'
+export const HARD_MODE_TEXT = '* Modo díficil activado'
