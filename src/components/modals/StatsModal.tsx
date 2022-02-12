a687c12c382354f4140ce4a803ff659b57bf7839
jtraverso <jtraverso@gmail.com>
2022-02-12 14:14:18 -0300

    ok

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -89,8 +89,6 @@ export const StatsModal = ({
             language: 'es',       // which language to use (see LANGUAGES)
             networks: [           // which networks to include (see SHARING NETWORKS)
               'whatsapp',
-			  'facebook',
-              'messenger',
               'twitter',
 			  'reddit',
             ],
@@ -102,35 +100,10 @@ export const StatsModal = ({
             // OPTIONAL PARAMETERS
             url: shareText(guesses, isGameLost, isHardMode), // (defaults to current url)
             //image: 'https://bit.ly/2CMhCMC',  // (defaults to og:image or twitter:image)
-            //description: shareText(guesses, isGameLost, isHardMode),       // (defaults to og:description or twitter:description)
+			//description: shareText(guesses, isGameLost, isHardMode),       // (defaults to og:description or twitter:description)
             title: 'Wordle.cl - Adivina la palabra en 6 intentos',            // (defaults to og:title or twitter:title)
           }}
         />
-	    <InlineShareButtons
-          config={{
-            alignment: 'center',  // alignment of buttons (left, center, right)
-            color: 'social',      // set the color of buttons (social, white)
-            enabled: true,        // show/hide buttons (true, false)
-            font_size: 16,        // font size for the buttons
-            labels: 'cta',        // button labels (cta, counts, null)
-            language: 'es',       // which language to use (see LANGUAGES)
-            networks: [           // which networks to include (see SHARING NETWORKS)
-			  'facebook',
-              'messenger',
-            ],
-            padding: 12,          // padding within buttons (INTEGER)
-            radius: 4,            // the corner radius on each button (INTEGER)
-            show_total: true,
-            size: 32,             // the size of each button (INTEGER)
-
-            // OPTIONAL PARAMETERS
-            //url: '', // (defaults to current url)
-            //image: '',  // (defaults to og:image or twitter:image)
-            description: shareText(guesses, isGameLost, isHardMode),       // (defaults to og:description or twitter:description)
-            title: 'Wordle.cl - Adivina la palabra en 6 intentos',            // (defaults to og:title or twitter:title)
-          }}
-        />
-        </div>
         </div>
 	    </div>
       )}
