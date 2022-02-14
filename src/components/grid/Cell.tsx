758261c7e0847d145339d0e466f6fcb229d5263d
jtraverso <jtraverso@users.noreply.github.com>
2022-02-14 14:33:16 -0300

    Update Cell.tsx

--- src/components/grid/Cell.tsx
+++ src/components/grid/Cell.tsx
@@ -1,7 +1,6 @@
 import { CharStatus } from '../../lib/statuses'
 import classnames from 'classnames'
 import { REVEAL_TIME_MS } from '../../constants/settings'
-import { getStoredIsHighContrastMode } from '../../lib/localStorage'
 
 type Props = {
   value?: string
