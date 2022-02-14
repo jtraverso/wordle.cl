bc963c2675fb19aaac56d264a9206cdbb36816b3
jtraverso <jtraverso@users.noreply.github.com>
2022-02-14 10:11:30 -0300

    Update words.ts

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -42,10 +42,10 @@ export const findFirstUnusedReveal = (word: string, guesses: string[]) => {
 export const getWordOfDay = () => {
   // January 1, 2022 Game Epoch
   const epochMs = new Date('January 1, 2022 00:00:00').valueOf()
-  const now = Date.now() - 10800000 //10.800.000 (3*60*60*1000) 3 hours less to the next play, so it suits better the chilean time
-  const msInDay = 28800000 //modified to play every 8 hours (8*60*60*1000)
+  const now = Date.now()
+  const msInDay = 43200000 //modified to play every 12 hours (12*60*60*1000)
   const index = Math.floor((now - epochMs) / msInDay)
-  const nextday = (index + 1) * msInDay + epochMs + 10800000  //10.800.000 (3*60*60*1000) 3 hours less to the next play, so it suits better the chilean time
+  const nextday = (index + 1) * msInDay + epochMs
 
   return {
     solution: WORDS[index % WORDS.length].toUpperCase(),
