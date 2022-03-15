ed9a84feec70872d017e12ad26020ffb30c085aa
jtraverso <jtraverso@users.noreply.github.com>
2022-03-15 15:05:05 -0300

    Update StatsModal.tsx

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -81,9 +81,16 @@ export const StatsModal = ({
           </button>
 	</div>
         <div className="mt-5 sm:mt-6 columns-1 dark:text-white">
+	  <SocialButtons
+	    guesses = {guesses}
+	    isGameLost = {isGameLost}
+	    isHardMode = {isHardMode}
+	 />
+        </div>
+        <div className="mt-5 sm:mt-6 columns-1 dark:text-white">
 	  <button
             type="button"
-            className="mt-2 w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+            className="mt-2 w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-400 text-base font-medium text-white hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:text-sm"
 	    onClick={() => {
     		const url = ABOUT_LINK(solution)
     		window.open(url, '_blank')
@@ -91,13 +98,6 @@ export const StatsModal = ({
           >
           {CL_TEXT(solution)}
           </button>
-        </div>
-        <div className="mt-5 sm:mt-6 columns-1 dark:text-white">
-	  <SocialButtons
-	    guesses = {guesses}
-	    isGameLost = {isGameLost}
-	    isHardMode = {isHardMode}
-	 />
         </div>
       </div>
       )}
