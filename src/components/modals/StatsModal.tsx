0b2014e346b9d10385d6819135c7ccb499894a69
jtraverso <jtraverso@users.noreply.github.com>
2022-03-15 14:02:25 -0300

    Update StatsModal.tsx

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -24,7 +24,6 @@ type Props = {
   isGameWon: boolean
   handleShare: () => void
   isHardMode: boolean
-  solution-text: solution
 }
 
 export const StatsModal = ({
@@ -36,7 +35,6 @@ export const StatsModal = ({
   isGameWon,
   handleShare,
   isHardMode,
-  solution,
 }: Props) => {
   if (gameStats.totalGames <= 0) {
     return (
@@ -87,7 +85,7 @@ export const StatsModal = ({
             type="button"
             className="mt-2 w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
 	    onClick={() => {
-    		const url = ABOUT_LINK(solution-text)
+    		const url = ABOUT_LINK(solution)
     		window.open(url, '_blank')
              }}
           >
