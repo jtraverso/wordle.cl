63790edb4162c621327b4277a9ead2c889e6293d
jtraverso <jtraverso@users.noreply.github.com>
2022-03-15 11:38:15 -0300

    Update StatsModal.tsx

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -82,8 +82,7 @@ export const StatsModal = ({
 	  <button
             type="button"
             className="mt-2 w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
-            {link = "https://about.wordle.cl/" + solution + ".html" }
-            onClick="window.location.href=\\"{link}\\" "
+            onClick="window.location.href=\"'https://about.wordle.cl/' + {solution} + '.html' + \""
             ¿Porqué {solution} es <img src="https://www.wordle.cl/logo192.png" width="32" height="32">
           </button>
         </div>
