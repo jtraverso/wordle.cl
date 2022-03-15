cae50dd10510005cc085c4b069ae7d2e4469258c
jtraverso <jtraverso@users.noreply.github.com>
2022-03-15 15:21:35 -0300

    Update StatsModal.tsx

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -63,7 +63,7 @@ export const StatsModal = ({
 	 <div className="mt-5 sm:mt-6 columns-1 dark:text-white">
 	  <button
             type="button"
-            className="mt-2 w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-500 text-base font-medium text-white hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm"
+            className="mt-2 w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
 	    onClick={() => {
     		const url = ABOUT_LINK(solution)
     		window.open(url, '_blank')
