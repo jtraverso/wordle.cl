eb36713b6b1338e0c304e43c3b5775b67c3cbf06
jtraverso <jtraverso@users.noreply.github.com>
2022-03-15 11:51:00 -0300

    Update StatsModal.tsx

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -82,8 +82,12 @@ export const StatsModal = ({
 	  <button
             type="button"
             className="mt-2 w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
-            onClick="window.location.href=\"'https://about.wordle.cl/' + {solution} + '.html' + \""
-            ¿Porqué {solution} es <img src="https://www.wordle.cl/logo192.png" width="32" height="32">
+	    onClick={(e) => {
+               e.preventDefault();
+               window.open= ('https://about.wordle.cl/' + {solution} + '.html' , "_blank")
+             }}
+          >
+          ¿Porqué {solution} es <img src="https://www.wordle.cl/logo192.png" width="32" height="32">
           </button>
         </div>
         <div className="mt-5 sm:mt-6 columns-1 dark:text-white">
